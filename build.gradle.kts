acd0d69aac43e5ce78638488c6557ec13ffe4b8a
ConstantinCezB <constantin.cez.b@gmail.com>
2020-08-01 00:31:11 -0400

    Upgrading dependencies.

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ buildscript {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.72")
         classpath("org.jetbrains.kotlin:kotlin-serialization:1.3.72")
         // Android tools
-        classpath("com.android.tools.build:gradle:4.2.0-alpha06")
+        classpath("com.android.tools.build:gradle:4.2.0-alpha07")
         // Navigation safe args
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.3.0")
         // Sqldelight
